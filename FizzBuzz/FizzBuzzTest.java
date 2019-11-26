c4d500b409d589b33f0c9dc341309dbf0dd11587
Caleb Reyes <sciencecamp123@gmail.com>
2019-11-25 17:56:08 -0800

    Added FizzBuzz Assignment

---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz iD = new FizzBuzz();
+
+        iD.fizzBuzz(5);
+        iD.fizzBuzz(3);
+        iD.fizzBuzz(15);
+        iD.fizzBuzz(2);
+        
+    }
+}
