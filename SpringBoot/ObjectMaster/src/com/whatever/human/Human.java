220379f91dbe9e7c1da91ded0ef1694968dd5f0d
Caleb Reyes <sciencecamp123@gmail.com>
2019-12-02 14:12:58 -0800

    Added ObjectMaster1 Assignment

---
+++ SpringBoot/ObjectMaster/src/com/whatever/human/Human.java
@@ -0,0 +1,62 @@
+package com.whatever.human;
+
+public class Human {
+	
+	public Human() {
+		
+	}
+	int strength = 3;
+	int intelligence = 3;
+	int stealth = 3;
+	int health = 100;
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getIntelligence() {
+		return intelligence;
+	}
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+	public int getStealth() {
+		return stealth;
+	}
+	public void setStealth(int stealth) {
+		this.stealth = stealth;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
+	
+	public void attack(Human human) {
+		int attackedHealth = human.getHealth();
+		int attackerStrength = this.getStrength();
+		attackedHealth -= attackerStrength;
+		human.setHealth(attackedHealth);
+		System.out.println("human has been attacked. Health is now down to " + attackedHealth + ".");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ SpringBoot/ObjectMaster/src/com/whatever/human/HumanTest.java
@@ -0,0 +1,25 @@
+package com.whatever.human;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human human1 = new Human();
+		Human human2 = new Human();
+		Human human3 = new Human();
+		
+		human1.attack(human2);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+		human2.attack(human1);
+	}
+
+}
