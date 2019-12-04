53393bfbbae1a3047bd7372535bc49d4debfbfaa
Caleb Reyes <sciencecamp123@gmail.com>
2019-12-03 19:27:00 -0800

    Added the UserInformation assignment

---
+++ servlets/SimpleProject/src/com/caleb/web/Home.java
@@ -0,0 +1,48 @@
+package com.caleb.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		String lang = request.getParameter("lang");
+		String town = request.getParameter("town");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.write("<h1>Welcome, " + firstName + " " + lastName + "</h1>" + "<h2> Your favorite language is " + lang + "</h2>" + "<h2>Your hometown is " + town + "</h2>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
